2fb9a9143ffad91d2efd1476fad7a602a7168ef3
SantiCarreno9 <santiago.carreno05@gmail.com>
2024-08-09 22:29:36 -0400

    Blazor: DealComparer site finished

--- PersonalFinanceApp.Web/Models/RegisterModel.cs
+++ PersonalFinanceApp.Web/Models/RegisterModel.cs
@@ -8,7 +8,7 @@ namespace PersonalFinanceApp.Web.Models
         public string EmailAddress { get; set; }
         [Required, DataType(DataType.Password), MinLength(7)]
         public string Password { get; set; }
-        [Required, DataType(DataType.Password), MinLength(7)]
+        [Required, DataType(DataType.Password), MinLength(7), Compare("Password",ErrorMessage ="Both passwords must match")]
         public string ConfirmPassword { get; set; }
     }
 }
