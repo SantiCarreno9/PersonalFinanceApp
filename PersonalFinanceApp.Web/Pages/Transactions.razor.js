4c8ec230b20135e50243c9ec776e5e3344b42060
SantiCarreno9 <santiago.carreno05@gmail.com>
2024-08-13 00:38:46 -0400

    Blazor: Transactions grid moved to its own component to reduce requests by handling rerendering

--- PersonalFinanceApp.Web/Pages/Transactions.razor.js
+++ PersonalFinanceApp.Web/Pages/Transactions.razor.js
@@ -1,10 +1,29 @@
-﻿const checkbox = document.getElementById('allCheckbox')
+﻿//const checkbox = document.getElementById('allCheckbox');
+//const deleteButton = document.getElementById('deleteButton');
 
-checkbox.addEventListener('change', (event) => {
-    const elements = document.getElementsByClassName("items-toggle");
-    const e = new Event('change');
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].checked = event.currentTarget.checked;
-        elements[i].dispatchEvent(new Event('change')/*e*/);
-    }    
-});
+//function onTransactionSelected(element) {
+//    if (element.checked) {
+//        deleteButton.disabled = false;
+//        element.parentelement.parentelement.classList.add("active");
+//    }        
+//    else {
+//        element.parentelement.parentelement.classList.remove("active");
+//        const elements = document.getElementsByClassName("items-toggle");        
+//        for (let i = 0; i < elements.length; i++) {
+//            if (elements[i].checked) {
+//                deleteButton.disabled = false;
+//                return;
+//            }                        
+//        }
+//        deleteButton.disabled = true;
+//    }
+//}
+
+//checkbox.addEventListener('change', (event) => {
+//    const elements = document.getElementsByClassName("items-toggle");
+//    const e = new Event('change');
+//    for (let i = 0; i < elements.length; i++) {
+//        elements[i].checked = event.currentTarget.checked;
+//        elements[i].dispatchEvent(new Event('change')/*e*/);
+//    }
+//});
